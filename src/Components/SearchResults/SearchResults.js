f276c12f59a3955366f5a020dc247309e5da143a
Roger Zavala <rogeliozavala15@gmail.com>
2020-03-13 09:06:17 -0700

    created new playlist component

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './SearchResults';
 
-class SearchResults extends React.Component{
+ class SearchResults extends React.Component{
     render(){
         return(
             <div className="SearchResults">
@@ -11,3 +11,5 @@ class SearchResults extends React.Component{
         );
     }
 }
+
+export default SearchResults;
