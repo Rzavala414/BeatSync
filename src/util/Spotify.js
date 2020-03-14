5c1a1927a183e1357cff0d7fd24c8bb03e0223a5
Roger Zavala <rogeliozavala15@gmail.com>
2020-03-14 16:29:33 -0700

    created an api call to spotify

--- src/util/Spotify.js
+++ src/util/Spotify.js
@@ -25,6 +25,10 @@ const Spotify = {
 
             window.location = accessURL;
         }
+    },
+
+    search(term) {
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, { headers: {Authorization: `Bearer ${accessToken}`}})
     }
 }
 
